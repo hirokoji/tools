7b0d8a62c856b7d54f12043ee158ae8b5f93ed91
Hiro Kojima <guitarsaiko@gmail.com>
2019-07-18 21:30:22 -0700

    Add function get vins

--- aws/check_cpu_utilization.sh
+++ aws/check_cpu_utilization.sh
@@ -5,18 +5,12 @@ if [ -z "$1" ]
     echo "Please specify cluster name like check_cpu_utilization.sh <cluster-name>"
     exit 1
 fi
-if [ -z "$2" ]
-  then
-    echo "Please specify service name like check_cpu_utilization.sh <service-name>"
-    exit 1
-fi
-
 
 currentTime=$(date -u +"%Y-%m-%dT%TZ")
 beforeTime=$(date -u -v -2M +"%Y-%m-%dT%TZ")
 clusterName=$1
-serviceName=$2
+#serviceName=$2
 #clusterName="drgdoesitwork-arms	"
-#serviceName="arms-cloud-service"
+serviceName="arms-cloud-service"
 
 aws cloudwatch get-metric-statistics --metric-name "CPUUtilization" --start-time $beforeTime --end-time $currentTime --period 60 --namespace "AWS/ECS" --statistics Average --dimensions Name=ServiceName,Value=$serviceName Name=ClusterName,Value=$clusterName |  jq '.Datapoints[0].Average'
--- aws/check_ecs_tasks.sh
+++ aws/check_ecs_tasks.sh
@@ -14,5 +14,10 @@ do
  taskArn=$(echo $taskinfo | jq  -r '.tasks[0].taskArn' | sed  s/arn.*task// | tr -d /)
  taskStatus=$(echo $taskinfo | jq  -r '.tasks[0].lastStatus')
  taskIP=$(echo $taskinfo | jq  -r '.tasks[0].containers[0].networkInterfaces[0].privateIpv4Address')
- echo "$taskArn $taskStatus $taskIP"
+ interface=$(echo $taskinfo | jq  -r '.tasks[0].attachments[0].details[1].value')
+ globalIP=$(aws ec2 describe-network-interfaces --network-interface-ids $interface | jq -r '.NetworkInterfaces[0].Association.PublicIp')
+ echo "- $taskArn $taskStatus $taskIP $globalIP $vins"
+ curl http://${globalIP}:22222/vins
+ echo ""
+ echo ""
 done
