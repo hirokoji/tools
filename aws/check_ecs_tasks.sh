ef821fd77b7e0a449fb98862eb0f42ee6006be3b
Hirotaka Kojima <guitarsaiko@gmail.com>
2019-09-16 09:10:14 +0900

    Add ecs service on check_ecs_task.sh

--- aws/check_ecs_tasks.sh
+++ aws/check_ecs_tasks.sh
@@ -17,11 +17,12 @@ for task in `aws ecs list-tasks --cluster $cluster  | jq -r '.taskArns | .[]'`
 do
  taskinfo=$(aws ecs describe-tasks --cluster $cluster --tasks $task)
  taskArn=$(echo $taskinfo | jq  -r '.tasks[0].taskArn' | sed  s/arn.*task// | tr -d /)
+ service=$(echo $taskinfo | jq  -r '.tasks[0].group' | awk -F: '{print $2}')
  taskStatus=$(echo $taskinfo | jq  -r '.tasks[0].lastStatus')
  taskIP=$(echo $taskinfo | jq  -r '.tasks[0].containers[0].networkInterfaces[0].privateIpv4Address')
  interface=$(echo $taskinfo | jq  -r '.tasks[0].attachments[0].details[1].value')
  globalIP=$(aws ec2 describe-network-interfaces --network-interface-ids $interface | jq -r '.NetworkInterfaces[0].Association.PublicIp')
- echo "- $taskArn $taskStatus $taskIP $globalIP $vins"
+ echo "- $taskArn $service $taskStatus $taskIP $globalIP $vins"
  curl http://${globalIP}:22222/vins
  echo ""
  echo ""
