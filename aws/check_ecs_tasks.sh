dcefb1eb55beb6dc6103c6aa0ff39e78b3fa34ae
Hirotaka Kojima <guitarsaiko@gmail.com>
2019-12-04 22:05:54 +0900

    Add total vins

--- aws/check_ecs_tasks.sh
+++ aws/check_ecs_tasks.sh
@@ -22,8 +22,13 @@ do
  taskIP=$(echo $taskinfo | jq  -r '.tasks[0].containers[0].networkInterfaces[0].privateIpv4Address')
  interface=$(echo $taskinfo | jq  -r '.tasks[0].attachments[0].details[1].value')
  globalIP=$(aws ec2 describe-network-interfaces --network-interface-ids $interface | jq -r '.NetworkInterfaces[0].Association.PublicIp')
- echo "- $taskArn $service $taskStatus $taskIP $globalIP $vins"
- curl http://${globalIP}:22222/vins
+ vins=$(curl -s http://${globalIP}:22222/vins)
+ totalvins=$(echo $vins | sed -e s/{\"vins\"://g | tr -d [  | tr -d ] | sed -e s/}// | awk -F ',' '{ print NF}')
+
+ echo "- $taskArn $service $taskStatus $taskIP $globalIP"
+ echo "  total vins: ${totalvins}"
+ echo "    $vins"
+
  echo ""
  echo ""
 done
